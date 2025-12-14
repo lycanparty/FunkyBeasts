2abab150d052abe6d2ba3ff8ab3cc6049d408976
Sunny/Felix <46765408+lycanparty@users.noreply.github.com>
2025-12-14 06:39:26 -0500

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -34,7 +34,7 @@ charadex.sheet = {
     inventory:     "inventory",
     inventoryLog:  "inventory log",
     items:         "items",
-    traits:        "traits",
+    traits:        "skills",
     prompts:       "prompts",
     faq:           "faq",
     staff:         "mods",
@@ -45,7 +45,7 @@ charadex.sheet = {
     designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
     statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
     rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Mammalian', 'Reptilian', 'Avian', 'Aquatic', 'Insectoid'],
+    species: ['All', 'Mammalian', 'Reptilian', 'Avian', 'Aquatic', 'Insectoid', 'Human'],
     itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
     traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
 
