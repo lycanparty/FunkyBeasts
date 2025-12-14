227e2e58f99fd67d7e3eb2c28f07888a3da06ec2
Sunny/Felix <46765408+lycanparty@users.noreply.github.com>
2025-12-14 04:25:13 -0500

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -26,7 +26,7 @@ charadex.site = {
 /* ==================================================================== */
 charadex.sheet = {
 
-  id: "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak",
+  id: "1cLGvSs_cz1KCd_xde8Vh_NHfNhdxgrZLe3lCMvpGp5I,
 
   pages: {
     masterlist:    "masterlist",
